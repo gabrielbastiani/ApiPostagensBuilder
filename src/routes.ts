23b302edbfd16160994392e10aacaa4f9c66c3b2
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-12-05 21:46:43 -0300

    'create'

--- src/routes.ts
+++ src/routes.ts
@@ -8,17 +8,22 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailuserController } from './controllers/user/DetailUserController';
 import { UpdateUserNameController } from './controllers/user/UpdateUserNameController';
 import { UpdateUserEmailController } from './controllers/user/UpdateUserEmailController';
+import { PhotoUpdateUserController } from './controllers/user/PhotoUpdateUserController';
 
 //POST
 import { CreatePostController } from './controllers/post/CreatePostController';
 import { UpdatePostDescriptionController } from './controllers/post/UpdatePostDescriptionController';
 import { UpdateImgPostPostController } from './controllers/post/UpdateImgPostPostController';
 
+//ANSWER
+import { CreateAnswerController } from './controllers/answer/CreateAnswerController';
+import { UpdateAnswerDescriptionController } from './controllers/answer/UpdateAnswerDescriptionController';
+import { UpdateImgAnswerAnswerController } from './controllers/answer/UpdateImgAnswerAnswerController';
+
 
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
-import { PhotoUpdateUserController } from './controllers/user/PhotoUpdateUserController';
 const router = Router();
 const upload = multer(uploadConfig.upload("./images"));
 
@@ -37,6 +42,11 @@ router.put('/photoUser', isAuthenticated, upload.single('file'), new PhotoUpdate
 router.post('/post', isAuthenticated, new CreatePostController().handle);
 router.put('/descriptionUpdate', isAuthenticated, new UpdatePostDescriptionController().handle);
 router.put('/imgPostUpdate', isAuthenticated, upload.single('file'), new UpdateImgPostPostController().handle);
+
+//ANSWER
+router.post('/answer', isAuthenticated, new CreateAnswerController().handle);
+router.put('/answerDesc', isAuthenticated, new UpdateAnswerDescriptionController().handle);
+router.put('/answerImg', isAuthenticated, new UpdateImgAnswerAnswerController().handle);
 
 
 export { router };
