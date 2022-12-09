cfc8cbf3306fb9dd73c676ca74004c0919e3b7df
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-12-08 21:16:27 -0300

    'finish'

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -45,7 +45,7 @@ class CreateUserService {
       }
     })
 
-    /* const transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
       host: process.env.HOST_SMTP,
       port: 465,
       auth: {
@@ -55,27 +55,27 @@ class CreateUserService {
     })
 
     await transporter.sendMail({
-      from: "'Blog - Builder Seu Negocio Online' <contato@builderseunegocioonline.com.br>",
+      from: "'Postagens - Builder Seu Negocio Online' <contato@builderseunegocioonline.com.br>",
       to: user.email,
-      subject: "Confirme seu cadastro de usuario no Blog",
+      subject: "Confirme seu cadastro de usuario na rede social da Builder Seu Negócio Online",
       html: `<div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
                 <h2>Confirme seu cadastro!</h2>
             </div>
             
             <article>
                 <p>Olá, ${user.name}!</p>
-                <p><a href="https://blog.builderseunegocioonline.com.br/userAuthenticated?user_id=${user.id}">CLIQUE AQUI</a>, para confirmar sua conta junto ao Blog e poder acessa-lo com os dados que cadastrou anteriormente.</p>
+                <p><a href="http://localhost:3333/authenticated?user_id=${user.id}">CLIQUE AQUI</a>, para confirmar sua conta junto a rede social, e poder acessa-la com os dados que cadastrou anteriormente.</p>
             </article>
             
             <div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
-                <h5>Blog Builder Seu Negocio Online</h5>
+                <h5>Postagens Builder Seu Negocio Online</h5>
             </div>`,
     });
 
     await transporter.sendMail({
-      from: "'Blog - Builder Seu Negocio Online' <contato@builderseunegocioonline.com.br>",
+      from: "'Postagens - Builder Seu Negocio Online' <contato@builderseunegocioonline.com.br>",
       to: "gabriel.bastiani@hotmail.com.br",
-      subject: "Novo usúario se cadastrando no blog",
+      subject: "Novo usúario se cadastrando na Rede Social da Builder Seu Negócio Online",
       html: `<div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
                 <h2>Novo usúario!</h2>
             </div>
@@ -86,9 +86,9 @@ class CreateUserService {
             </article>
             
             <div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
-                <h5>Blog Builder Seu Negocio Online</h5>
+                <h5>Postagens Builder Seu Negocio Online</h5>
             </div>`,
-    }); */
+    });
 
     return user;
   }
